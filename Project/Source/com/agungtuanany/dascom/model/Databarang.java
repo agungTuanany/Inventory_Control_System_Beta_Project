61b4b615d0b7364e53f4b28033b904f7cafde676
Agung Tuanany <agung.tuanany@gmail.com>
2015-08-04 18:07:39 +0700

    Create Databarang.java

---
+++ Project/Source/com/agungtuanany/dascom/model/Databarang.java
@@ -0,0 +1,78 @@
+package com.agungtuanany.dascom.model;
+
+public class Databarang {
+
+	private String idDatabarang;
+	private String namabarang;
+	private int stok;
+	private double hargajual;
+	private double hargabeli;
+	private String satuanukuran;
+	private String deksripsi;
+	private String keterangan;
+
+	public final String getIdDatabarang() {
+		return idDatabarang;
+	}
+
+	public final void setIdDatabarang(String idDatabarang) {
+		this.idDatabarang = idDatabarang;
+	}
+
+	public final String getNamabarang() {
+		return namabarang;
+	}
+
+	public final void setNamabarang(String namabarang) {
+		this.namabarang = namabarang;
+	}
+
+	public final int getStok() {
+		return stok;
+	}
+
+	public final void setStok(int stok) {
+		this.stok = stok;
+	}
+
+	public final double getHargajual() {
+		return hargajual;
+	}
+
+	public final void setHargajual(double hargajual) {
+		this.hargajual = hargajual;
+	}
+
+	public final double getHargabeli() {
+		return hargabeli;
+	}
+
+	public final void setHargabeli(double hargabeli) {
+		this.hargabeli = hargabeli;
+	}
+
+	public final String getSatuanukuran() {
+		return satuanukuran;
+	}
+
+	public final void setSatuanukuran(String satuanukuran) {
+		this.satuanukuran = satuanukuran;
+	}
+
+	public final String getDeksripsi() {
+		return deksripsi;
+	}
+
+	public final void setDeksripsi(String deksripsi) {
+		this.deksripsi = deksripsi;
+	}
+
+	public final String getKeterangan() {
+		return keterangan;
+	}
+
+	public final void setKeterangan(String keterangan) {
+		this.keterangan = keterangan;
+	}
+
+}
